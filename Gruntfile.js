3777354832160cf407c89b95a9f762c11d2fe82f
m-amaya <marissa.amaya.311@gmail.com>
2015-07-15 20:51:36 -0500

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+  
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "public/build/style.css": "public/src/css/style.less"
+        }
+      }
+    },
+    
+    browserify: {
+      options: {
+        transform: ['reactify', 'debowerify']
+      },
+      dev: {
+        options: {
+          debug: true
+        },
+        src: 'public/src/js/main.jsx',
+        dest: 'public/build/bundle.js'
+      },
+      production: {
+        options: {
+          debug: false
+        },
+        src: '<%= browserify.dev.src %>',
+        dest: 'public/build/bundle.js'
+      }
+    },
+    
+    watch: {
+      styles: {
+        files: ['public/src/css/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      },
+      browserify: {
+        files: 'public/src/js/*.jsx',
+        tasks: ['browserify:dev']
+      }
+    }
+  });
+  
+  grunt.registerTask('default', ['less', 'browserify:dev', 'watch']);
+  grunt.registerTask('package', ['less', 'browserify:production']);
+  
+};
