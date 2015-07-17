6cd3542d02aa7d18a3ee2af4c7f63464be2c8247
m-amaya <marissa.amaya.311@gmail.com>
2015-07-16 21:45:28 -0500

    Updated Gruntfile to include uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   require('jit-grunt')(grunt);
-  
+
   grunt.initConfig({
     less: {
       development: {
@@ -10,11 +10,21 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "public/build/style.css": "public/src/css/style.less"
+          "public/build/style.min.css": "public/src/css/*.less"
         }
       }
     },
-    
+
+    uglify: {
+      options: {
+        banner: '/*! Grunt Uglify <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      build: {
+        src: 'public/build/bundle.js',
+        dest: 'public/build/bundle.min.js'
+      }
+    },
+
     browserify: {
       options: {
         transform: ['reactify', 'debowerify']
@@ -23,18 +33,25 @@ module.exports = function(grunt) {
         options: {
           debug: true
         },
-        src: ['public/src/js/*.js', 'public/src/views/*.jsx'],
+        src: ['public/src/js/*.js', 'public/src/js/view/*.jsx'],
         dest: 'public/build/bundle.js'
       },
       production: {
         options: {
           debug: false
         },
-        src: '<%= browserify.dev.src %>',
+        src: [
+          'public/libs/underscore/underscore-min.js',
+          'public/libs/jquery/dist/jquery.min.js',
+          'public/libs/d3/d3.min.js',
+          'public/libs/react/react.min.js',
+          'public/libs/backbone/backbone-min.js',
+          '<%= browserify.dev.src %>'
+        ],
         dest: 'public/build/bundle.js'
       }
     },
-    
+
     watch: {
       styles: {
         files: ['public/src/css/*.less'],
@@ -44,13 +61,13 @@ module.exports = function(grunt) {
         }
       },
       browserify: {
-        files: 'public/src/views/*.jsx',
+        files: 'public/src/js/view/*.jsx',
         tasks: ['browserify:dev']
       }
     }
   });
-  
-  grunt.registerTask('default', ['less', 'browserify:dev', 'watch']);
-  grunt.registerTask('package', ['less', 'browserify:production']);
-  
+
+  grunt.registerTask('default', ['less', 'browserify:dev', 'uglify', 'watch']);
+  grunt.registerTask('package', ['less', 'browserify:production', 'uglify']);
+
 };
--- app/db/db.js
+++ app/db/db.js
@@ -2,43 +2,43 @@ var pg = require('pg');
 var uuid = require('node-uuid');
 
 module.exports = {
-  getPolls: function(req, res, client) {
+  getPolls: function (req, res, client) {
     console.log(req.originalMethod + ' ' + req.originalUrl);
     var query = client.query("SELECT * from polls");
-    query.on("row", function(row, result) {
+    query.on("row", function (row, result) {
       result.addRow(row);
     });
-    query.on("end", function(result) {
+    query.on("end", function (result) {
       res.json(result.rows);
     });
-    query.on("error", function(error) {
+    query.on("error", function (error) {
       res.json(error);
     });
   },
-  addPoll: function(req, res, client) {
+  addPoll: function (req, res, client) {
     console.log(req.originalMethod + ' ' + req.originalUrl);
     var query = client.query("INSERT INTO polls \
       (uuid, question, opt1, opt2, opt3) \
-      VALUES ('"+uuid.v1()+"', '"+req.body.question+"', \
-      '"+req.body.opt1+"', '"+req.body.opt2+"', '"+req.body.opt3+"')");
-    query.on("end", function(result) {
+      VALUES ('" + uuid.v1() + "', '" + req.body.question + "', \
+      '" + req.body.opt1 + "', '" + req.body.opt2 + "', '" + req.body.opt3 + "')");
+    query.on("end", function (result) {
       res.json(result);
     });
-    query.on("error", function(error) {
+    query.on("error", function (error) {
       res.json(error);
     });
   },
-  delPoll: function(req, res, client) {
+  delPoll: function (req, res, client) {
     console.log(req.originalMethod + ' ' + req.originalUrl);
-    var query = client.query("DELETE FROM polls WHERE uuid = '"+req.params.id+"'");
-    query.on("end", function(result) {
+    var query = client.query("DELETE FROM polls WHERE uuid = '" + req.params.id + "'");
+    query.on("end", function (result) {
       res.json(result);
     });
-    query.on("error", function(error) {
+    query.on("error", function (error) {
       res.json(error);
     });
   },
-  updatePoll: function(req, res, client) {
+  updatePoll: function (req, res, client) {
     console.log(req.originalMethod + ' ' + req.originalUrl);
     // grab data
     var votefor = {
@@ -46,36 +46,36 @@ module.exports = {
       opt2: req.body.opt2,
       opt3: req.body.opt3
     }
-    
+
     if (votefor.opt1) {
       console.log("Option 1 voted on.");
-      var query = client.query("UPDATE polls SET opt1votes = opt1votes + 1 WHERE uuid = '"+req.params.id+"'");
-      query.on("end", function(result) {
+      var query = client.query("UPDATE polls SET opt1votes = opt1votes + 1 WHERE uuid = '" + req.params.id + "'");
+      query.on("end", function (result) {
         res.json(result);
       });
-      query.on("error", function(error) {
+      query.on("error", function (error) {
         res.json(error);
       });
     }
-    
+
     if (votefor.opt2) {
       console.log("Option 2 voted on.");
-      var query = client.query("UPDATE polls SET opt2votes = opt2votes + 1 WHERE uuid = '"+req.params.id+"'");
-      query.on("end", function(result) {
+      var query = client.query("UPDATE polls SET opt2votes = opt2votes + 1 WHERE uuid = '" + req.params.id + "'");
+      query.on("end", function (result) {
         res.json(result);
       });
-      query.on("error", function(error) {
+      query.on("error", function (error) {
         res.json(error);
       });
     }
-    
+
     if (votefor.opt3) {
       console.log("Option 3 voted on.");
-      var query = client.query("UPDATE polls SET opt3votes = opt3votes + 1 WHERE uuid = '"+req.params.id+"'");
-      query.on("end", function(result) {
+      var query = client.query("UPDATE polls SET opt3votes = opt3votes + 1 WHERE uuid = '" + req.params.id + "'");
+      query.on("end", function (result) {
         res.json(result);
       });
-      query.on("error", function(error) {
+      query.on("error", function (error) {
         res.json(error);
       });
     }
