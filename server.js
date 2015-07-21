6334ef29bf83bfd9475ccf4d7a131bb7bebdeed3
m-amaya <marissa.amaya.311@gmail.com>
2015-07-20 19:03:21 -0500

    Readying app for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express              = require('express');
 var app                  = express();
 var bodyParser           = require('body-parser');
 var methodOverride       = require('method-override');
+var pg                   = require('pg');
 
 // configuration ======================================
 //var db = require('./app/db/config');
